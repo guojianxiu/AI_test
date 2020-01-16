dc7681cd0563e2c5438c637b32540b14efa7eeb6
zhjfeng <15850512966@163.com>
2020-01-16 13:49:39 +0800

    习题点击

--- page/exercise.py
+++ page/exercise.py
@@ -7,21 +7,26 @@ class Exercisepage(Page):
     question_loc = PageElement(class_name = 'slick-current')
     question_list_loc = PageElements(class_name = 'slick - slide')
     subit_loc = PageElement(class_name = 'submit_btn')
-    x= 1
-    selects_loc1 = PageElement(xpath='/html/body/div[1]/div/div/div[3]/div[1]/div/div/div/div[' + str(x) + ']/div/div/div[3]/span[1]')
+    #selects_loc = PageElement(xpath='/html/body/div[1]/div/div/div[3]/div[1]/div/div/div/div[' + str(x) + ']/div/div/div[3]/span[1]')
+    selects_loc = PageElement(css = '.slick-active .option:nth-child(2) > .num')
 
     def do_exercise(self):
         self.question_loc.click()
+        for x in range(0,10):
+            self.selects_loc.click()
+            time.sleep(2)
+        time.sleep(20)
 
+
+        '''
         for x in range(1,11):
             #异常
             #y = (random.randint(2, 5))
-            self.selects_loc = PageElement(xpath='/html/body/div[1]/div/div/div[3]/div[1]/div/div/div/div[' + str(x) + ']/div/div/div[3]/span[1]')
-            print(self.selects_loc)
+            selects_loc = PageElement(xpath='/html/body/div[1]/div/div/div[3]/div[1]/div/div/div/div[' + str(x) + ']/div/div/div[3]/span[1]')
             self.selects_loc.click()
             time.sleep(2)
 
-    '''
+    
     正常
         self.question_loc.click()
         print(self.selects_loc1)
